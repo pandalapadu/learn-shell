998a261e2fb63dfa8c0161811f1bb179d2a4e25e
Venkat Kunam <venkat@gmail.com>
2025-12-20 13:32:53 +0530

    Robo Shop Project Frontend file created

---
+++ Roboshop-Shell/01-Frontend.sh
@@ -0,0 +1,19 @@
+###Install Nginx
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+##Start & Enable Nginx service
+systemctl enable nginx
+systemctl start nginx
+#Remove the default content that web server is serving.
+rm -rf /usr/share/nginx/html/*
+#Download the frontend content
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+#Extract the frontend content.
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+#Create Nginx Reverse Proxy Configuration to reach backend services.
+cp nginx.conf /etc/nginx/nginx.conf
+##Restart Nginx Service to load the changes of the configuration.
+systemctl restart nginx
