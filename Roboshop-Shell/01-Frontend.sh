978eee752122d23b9b81ab5d55292adb2ebd3564
Venkat Kunam <venkat@gmail.com>
2025-12-20 14:10:15 +0530

    Robo Shop Project MongoDB file updated

--- Roboshop-Shell/01-Frontend.sh
+++ Roboshop-Shell/01-Frontend.sh
@@ -3,7 +3,7 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 #Create Nginx Reverse Proxy Configuration to reach backend services.
-cp nginx.conf /etc/nginx/nginx.conf
+cp 01-Frontend.nginx.conf /etc/nginx/nginx.conf
 ##Start & Enable Nginx service
 systemctl enable nginx
 systemctl start nginx
