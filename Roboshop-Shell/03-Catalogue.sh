8abd25d6173ee905316795f323a2cc3c91947ac6
Venkat Kunam <venkat@gmail.com>
2025-12-21 20:15:42 +0530

    Robo Shop Project Updated the Catalogue DNS Url in the nginx conf file

---
+++ Roboshop-Shell/03-Catalogue.sh
@@ -0,0 +1,34 @@
+#Install NodeJS, By default NodeJS 16 is available, We would like to enable 20 version and install list.
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+#Install NodeJS
+dnf install nodejs -y
+#Configure the application.
+#Add application User
+useradd roboshop
+#Lets setup an app directory.
+mkdir /app
+#Download the application code to created app directory.
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+#Lets download the dependencies.
+cd /app
+npm install
+#Setup SystemD Catalogue Service
+cp 03.catalogue.service /etc/systemd/system/catalogue.service
+
+#Load the service.
+systemctl daemon-reload
+
+#Start the service.
+systemctl enable catalogue
+systemctl start catalogue
+
+#mongo client installed we have to setup MongoDB repo and install mongodb-client
+
+cp 03.mongodb-client /etc/yum.repos.d/mongo.repo
+#install mongodb-client
+dnf install mongodb-mongosh -y
+#Load Master Data of the List of products we want to sell and their quantity information also there in the same master data.
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
